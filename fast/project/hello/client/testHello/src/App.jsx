854e55f3ca5dbdd86edb5a2de49a89aa4ea17dda
kimdongyoon488 <rlaehddbs54321@gmail.com>
2025-05-08 11:36:29 +0900

    WebSocket 실시간 통신 서버 구현

--- fast/project/hello/client/testHello/src/App.jsx
+++ fast/project/hello/client/testHello/src/App.jsx
@@ -7,7 +7,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <a href="http://localhost:8000">fast api test </a>
+    <>
+        <a href="http://localhost:8000">fast api test </a>
+        <form method="post" action="http://localhost:8000/items/2">
+          <input name="q"></input>
+          <button type="submit">제출</button>
+        </form>
+    </>  
   )
 }
 
