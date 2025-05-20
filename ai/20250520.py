44d1ae2538fcf732d11391e02a26a9bcb26bf5cf
kimdongyoon488 <rlaehddbs54321@gmail.com>
2025-05-20 16:28:19 +0900

    iris data를 보고 품종을 예측하는 모델 개발 후 성능 평가

--- ai/20250520.py
+++ ai/20250520.py
@@ -1,4 +1,7 @@
 from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+
 
 import pandas as pd
 import seaborn as sns
@@ -43,24 +46,74 @@ iris["species"] = \
 
 
 #(sepal/petal의 길이·너비) 데이터들을 boxplot을 사용하여 시각화
-def boxplot_iris(feature_names, dataset):
-    i = 1
-    plt.figure(figsize=(11,9))
-    for col in feature_names:
-        plt.subplot(2, 2, i)
-        plt.axis('on')
-        plt.tick_params(axis='both', left=True,
-                        top=False, right=False,
-                        bottom=True, labelleft=False,
-                        labeltop=False, labelright=False,
-                        labelbottom=False)
-        dataset[col].plot(kind='box', subplots=True,
-                          sharex=False, sharey=False)
-        plt.title(col)
-        i += 1
-    plt.show()
+# def boxplot_iris(feature_names, dataset):
+#     i = 1
+#     plt.figure(figsize=(11,9))
+#     for col in feature_names:
+#         plt.subplot(2, 2, i)
+#         plt.axis('on')
+#         plt.tick_params(axis='both', left=True,
+#                         top=False, right=False,
+#                         bottom=True, labelleft=False,
+#                         labeltop=False, labelright=False,
+#                         labelbottom=False)
+#         dataset[col].plot(kind='box', subplots=True,
+#                           sharex=False, sharey=False)
+#         plt.title(col)
+#         i += 1
+#     plt.show()
 
-boxplot_iris(iris.columns[:-1], iris)
+# boxplot_iris(iris.columns[:-1], iris)
+
+# def histogram_iris(feature_names, dataset):
+#     i = 1
+#     plt.figure(figsize=(11,9))
+#     for col in feature_names:
+#         plt.subplot(2, 2, i)
+#         plt.axis('on')
+#         plt.tick_params(axis='both', left=True,
+#                         top=False, right=False,
+#                         bottom=True, labelleft=False,
+#                         labeltop=False, labelright=False,
+#                         labelbottom=False)
+#         dataset[col].hist()
+#         plt.title(col)
+#         i += 1
+#     plt.show()
+
+# histogram_iris(iris.columns[:-1], iris)
+
+
+
+
+# ========== 머신러닝 모델 ==============
+
+X_train, X_test, y_train, y_test = \
+    train_test_split(iris.iloc[:, :-1], iris.iloc[:, -1],
+                     test_size=0.33, random_state=42)
+
+
+model = DecisionTreeClassifier(
+    criterion='gini',
+    splitter='best',
+    max_depth=None,
+    min_samples_split=2,
+    min_samples_leaf=1,
+    min_weight_fraction_leaf=0.0,
+    max_features=None,
+    random_state=42,
+    max_leaf_nodes=None,
+    min_impurity_decrease=0.0,
+    class_weight=None,
+)
+
+
+#학습
+model.fit(X_train, y_train)
+
+#성능 평가
+print(model.score(X_test, y_test))
+
 
 
 
